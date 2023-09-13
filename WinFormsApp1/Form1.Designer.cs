a367542b7a187788750ffec9c7d50cd1e1d1afbe
Michał Grundemann <michal.grundemann@teb.edu.pl>
2023-09-13 12:23:30 +0200

    gierka przycsik zadanie

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,34 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.BackColor = Color.OrangeRed;
-            button1.Location = new Point(297, 156);
+            button1.BackColor = Color.Lime;
+            button1.Font = new Font("Arial ", 20F, GraphicsUnit.Point);
+            button1.Location = new Point(927, 435);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(126, 59);
+            button1.Size = new Size(152, 48);
             button1.TabIndex = 0;
-            button1.Text = "złapmnie";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += buttonClick;
+            button1.Text = "catch me!";
+            button1.Click += button1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 3000;
+            timer1.Tick += timer1_Tick_1;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 596);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1159, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(56, 17);
+            toolStripStatusLabel1.Text = "Punkty: 0";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 14F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(801, 453);
+            ClientSize = new Size(1159, 618);
+            Controls.Add(statusStrip1);
             Controls.Add(button1);
+            Font = new Font("Algerian", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
-            Load += Form1_Load;
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,20 +1,37 @@
+using System.Windows.Forms;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        Random r;
+        int punkty;
         public Form1()
         {
+
             InitializeComponent();
+            r = new Random();
+            punkty = 0;
+            timer1.Start();
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            MoveButton();
+            punkty = 0;
+            toolStripStatusLabel1.Text = "Punkty: " + punkty.ToString();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
-        }
-
-        private void buttonClick(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            //Application.Exit();
+            MoveButton();
+            punkty++;
+            toolStripStatusLabel1.Text = "Punkty: " + punkty.ToString();
+            timer1.Stop();
+            timer1.Start();
+
+
+            /*//Application.Exit();
 
             //szerokosc
             int windowWidth = Size.Width;
@@ -30,9 +47,20 @@ namespace WinFormsApp1
             int RandomTop = rnd.Next(windowHeight);
 
             Point topLeftCorner = new Point(RandomLeft, RandomTop);
-            button1.Location = topLeftCorner;
+            button1.Location = topLeftCorner;   
+            */
+        }
 
 
+
+        private void MoveButton()
+        {
+            int maxX = this.Size.Width - button1.Size.Width - 50;
+            int maxY = this.Size.Height - button1.Size.Height - 50;
+            Point p = new Point();
+            p.X = r.Next(12, maxX);
+            p.Y = r.Next(12, maxY);
+            button1.Location = p;
         }
     }
 }
